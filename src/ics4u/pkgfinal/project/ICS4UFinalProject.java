fb123eb18cc906f41c5ba07b3f7e7d84ddd5aa12
Sergiobix <50592047+Sergiobix@users.noreply.github.com>
2019-05-23 13:14:22 -0400

    Class creation

--- src/ics4u/pkgfinal/project/ICS4UFinalProject.java
+++ src/ics4u/pkgfinal/project/ICS4UFinalProject.java
@@ -17,7 +17,7 @@ public class ICS4UFinalProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
-       
+        System.out.println("LOL");
     }
     
 }
